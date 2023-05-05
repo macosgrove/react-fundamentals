96539d095ade7d36c06a14f94d0d223e57b6fdf3
macosgrove <maryanne@covidence.org>
2023-05-05 16:52:20 +1000

    Exercise 6 basic

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -19,11 +19,18 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const username = event.target.elements.username.value
+    onSubmitUsername(username);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
